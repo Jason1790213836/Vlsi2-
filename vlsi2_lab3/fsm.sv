836085cb1d5f79ad4e8a62e03fb9e22ed3113551
lx2349 <lx2349@ecs04.poly.edu>
2026-03-05 15:17:03 -0500

    fsm1.sv

--- vlsi2_lab3/fsm.sv
+++ vlsi2_lab3/fsm.sv
@@ -9,7 +9,7 @@ output logic y,
 output state˙e state
 );
 
-	always@(posedge clk or negedge rst_n)begin
+	always_ff@(posedge clk or negedge rst_n)begin
 		if(!rst_n)begin
 			state<=state.S0;
 			y<=0;
