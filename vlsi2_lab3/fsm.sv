8160449a20a028d18c24703e19f78db71be93413
lx2349 <lx2349@ecs04.poly.edu>
2026-03-05 14:31:25 -0500

    good

---
+++ vlsi2_lab3/fsm.sv
@@ -0,0 +1,12 @@
+`include ”fsm˙pkg.svh”
+import fsm10˙pkg::*;
+module fsm(
+input logic rst˙n, // asynchronous active-low reset
+input logic clk,
+input logic jmp,
+input logic go,
+output logic y,
+output state˙e state
+);
+// Implement the design
+endmodule
