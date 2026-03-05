83a4ddf156cfab75d8289118ed0be3f562710883
lx2349 <lx2349@ecs04.poly.edu>
2026-03-05 15:12:08 -0500

    fsm.sv

--- vlsi2_lab3/fsm.sv
+++ vlsi2_lab3/fsm.sv
@@ -8,5 +8,106 @@ input logic go,
 output logic y,
 output state˙e state
 );
+
+	always@(posedge clk or negedge rst_n)begin
+		if(!rst_n)begin
+			state<=state.S0;
+			y<=0;
+		end
+		case(state)
+			state.S0:begin
+				 if(!go)begin
+					state<=state.S0;
+					y<=0;
+				 end else if (go==1&&!jmp)begin
+					state<=state.S1;
+					y<=0;
+				 end else if (go==1&&jmp)begin
+					state<=state.S3;
+					y<=1;
+				 end
+			end
+                        state.S1:begin
+                          	 if (!jmp)begin
+                                        state<=state.S2;
+                                        y<=0;
+                                 end else if (jmp)begin
+                                        state<=state.S3;
+                                        y<=1;
+                                 end
+                        end
+                        state.S2:begin
+                              
+                                        state<=state.S3;
+                                        y<=1;
+                          
+                        end
+                        state.S3:begin
+                                if (!jmp)begin
+                                        state<=state.S4;
+                                        y<=0;
+
+                        end
+                        state.S4:begin
+                                if (!jmp)begin
+                                        state<=state.S5;
+                                        y<=0;
+                                end else if (jmp)begin
+                                        state<=state.S3;
+                                        y<=1;
+                                end
+                        end
+                        state.S5:begin
+                                if (!jmp)begin
+                                        state<=state.S6;
+                                        y<=0;
+                                end else if (jmp)begin
+                                        state<=state.S3;
+                                        y<=1;
+                                end
+                        end
+                        state.S6:begin
+                                if (!jmp)begin
+                                        state<=state.S7;
+                                        y<=0;
+                                end else if (jmp)begin
+                                        state<=state.S3;
+                                        y<=1;
+                                end
+                        end
+                        state.S7:begin
+                               if (!jmp)begin
+                                        state<=state.S8;
+                                        y<=0;
+                                end else if (jmp)begin
+                                        state<=state.S3;
+                                        y<=1;
+                                end
+                        end
+                        state.S8:begin
+                       		if (!jmp)begin
+                                        state<=state.S9;
+                                        y<=0;
+                                end else if (jmp)begin
+                                        state<=state.S3;
+                                        y<=1;
+                                end
+                        end
+                        state.S9:begin
+                                if (!jmp)begin
+                                        state<=state.S0;
+                                        y<=0;
+                                end else if (jmp)begin
+                                        state<=state.S3;
+                                        y<=1;
+                                end
+                        end
+			default: begin stat3<=0; 
+					y<=0
+				 end
+
+		endcase
+
+	end
 // Implement the design
 endmodule
