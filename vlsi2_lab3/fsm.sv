1c31bdd04117fea009b7335eac76fffe3c6b02db
lx2349 <lx2349@ecs04.poly.edu>
2026-03-05 22:30:32 -0500

    main

--- vlsi2_lab3/fsm.sv
+++ vlsi2_lab3/fsm.sv
@@ -9,70 +9,78 @@ module fsm(
     output logic   y,
     output state_e state
 );
-
+    state_e next_state;
+    assign y=(state==S3);
     // Moore output: y depends only on state (y=1 only in S3)
-    assign y = (state == S3);
+    always_comb begin
 
+	next_state=state;
+
+	case(state)
+	      S0:begin
+			if(!go)begin
+				next_state=S0;
+			end else if(go&&!jmp)begin
+				next_state=S1;
+			end else if (go&&jmp)begin
+				next_state=S3;
+			end
+		end
+             S1: begin
+                if (!jmp) next_state = S2;
+                else      next_state = S3;
+            end
+
+            S2: begin
+                next_state = S3;
+            end
+
+            S3: begin
+                if (!jmp) next_state = S4;
+                else      next_state = S3;
+            end
+
+            S4: begin
+                if (!jmp) next_state = S5;
+                else      next_state = S3;
+            end
+
+            S5: begin
+                if (!jmp) next_state = S6;
+                else      next_state = S3;
+            end
+
+            S6: begin
+                if (!jmp) next_state = S7;
+                else      next_state = S3;
+            end
+
+            S7: begin
+                if (!jmp) next_state = S8;
+                else      next_state = S3;
+            end
+
+            S8: begin
+                if (!jmp) next_state = S9;
+                else      next_state = S3;
+            end
+
+            S9: begin
+                if (!jmp) next_state = S0;
+                else      next_state = S3;
+            end
+
+            default: begin
+                next_state = S0;
+            end
+	endcase
+    end
     always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            state <= S0;
-        end else begin
-            unique case (state)
-                S0: begin
-                    if (!go)       state <= S0;
-                    else if (!jmp) state <= S1;
-                    else           state <= S3;  // go & jmp
-                end
-
-                S1: begin
-                    if (!jmp) state <= S2;
-                    else      state <= S3;
-                end
-
-                S2: begin
-                    state <= S3;
-                end
-
-                S3: begin
-                    if (!jmp) state <= S4;
-                    else      state <= S3;  // jmp stays in S3
-                end
-
-                S4: begin
-                    if (!jmp) state <= S5;
-                    else      state <= S3;
-                end
-
-                S5: begin
-                    if (!jmp) state <= S6;
-                    else      state <= S3;
-                end
-
-                S6: begin
-                    if (!jmp) state <= S7;
-                    else      state <= S3;
-                end
-
-                S7: begin
-                    if (!jmp) state <= S8;
-                    else      state <= S3;
-                end
-
-                S8: begin
-                    if (!jmp) state <= S9;
-                    else      state <= S3;
-                end
-
-                S9: begin
-                    if (!jmp) state <= S0;
-                    else      state <= S3;
-                end
-
-                default: begin
-                    state <= S0;
-                end
-            endcase
-        end
+	    if(!rst_n)begin
+                 state<=S0;
+	    end else begin
+		    state<=next_state;
+	 end
     end
 
 endmodule
