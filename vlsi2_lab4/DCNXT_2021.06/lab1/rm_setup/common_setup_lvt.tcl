744976c01334891817320cba3c9b5716da4e90ff
lx2349 <lx2349@ecs04.poly.edu>
2026-04-02 12:07:14 -0400

    new

---
+++ vlsi2_lab4/DCNXT_2021.06/lab1/rm_setup/common_setup_lvt.tcl
@@ -0,0 +1,27 @@
+##########################################################################################
+# User-defined variables for logical library setup in dc_setup.tcl
+##########################################################################################
+
+#set PDK_DIR "/ip/synopsys/saed32/v02_2024"
+
+set ADDITIONAL_SEARCH_PATH   "../ref/DBs ../ref/CLIBs ../ref/tech  ./rtl ./scripts"  ;#  Directories containing logic libraries,
+                                                                                       #  logic design and script files.
+#set ADDITIONAL_SEARCH_PATH   "$PDK_DIR/lib/stdcell_lvt/db_nldm $PDK_DIR/tech/tf $PDK_DIR/tech/starrc/max $PDK_DIR/tech/starrc $PDK_DIR/temporary_ndm ./rtl ./scripts" ;
+
+set TARGET_LIBRARY_FILES     "saed32lvt_ss0p75v125c.db"                              ;#  Logic cell library files
+
+##########################################################################################
+# User-defined variables for physical library setup in dc_setup.tcl
+##########################################################################################
+
+set NDM_DESIGN_LIB           "TOP_lvt.dlib"                 ;#  User-defined NDM design library name
+
+set NDM_REFERENCE_LIBS       "saed32_lvt.ndm"                 ;#  NDM physical cell libraries
+
+set TECH_FILE                "saed32nm_1p9m.tf"              ;#  Technology file
+
+set TLUPLUS_MAX_FILE         "saed32nm_1p9m_Cmax.tluplus"    ;#  Max TLUPlus file
+
+set MAP_FILE                 "saed32nm_tf_itf_tluplus.map"   ;#  Mapping file for TLUplus
+
+return
