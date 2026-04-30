d247b97dd6d369a9d4524c92d9f9c1db6588bc61
lx2349 <lx2349@ecs04.poly.edu>
2026-04-29 21:21:36 -0400

    ne

---
+++ run_lvs/svdb/inv_x1/rex_rcx.sh
@@ -0,0 +1,9 @@
+set -e
+set -x
+
+rex -dp_comm_string 1,ecs04.poly.edu,38139 -V -m -pd -I# -tech /ip/cadence/gpdk045/gpdk045_v_6_0/qrc/rcworst -map p2elayermapfile -wee p2elayermapfile -N NET -Ply np_rpoly_0conn -er np_rmetal1_0conn,metal1_conn.den -rP res.mod -mp mprexaZOthHY np_rmetal1_0conn::metal1_conn_cut - rcont_0poly,1,T rcont_0pdiff_metal1_0conn_pdiff_0conn,1,t rcont_0ndiff_metal1_0conn_ndiff_0conn,1,t - L1T0,1,I
+
+rex -dp_comm_string 2,ecs04.poly.edu,38139 -V -m -pd -I# -tech /ip/cadence/gpdk045/gpdk045_v_6_0/qrc/rcworst -map p2elayermapfile -wee p2elayermapfile -N NET -Ply np_rpoly_0conn -er np_rmetal1_0conn,metal1_conn.den -rP res.mod -mp mprexaqY4vAr np_rpoly_0conn::poly_conn_cut - _0pmos1v_mgvia,1,z _0nmos1v_mgvia,1,z rcont_0poly,1,x
+
+rexmerge -V -N NET -ls slab -n mprexaqY4vAr,mprexaZOthHY -b np_rpoly_0conn,np_rmetal1_0conn -l ,L1T0 np_rpoly_0conn.res,np_rmetal1_0conn.res
+
